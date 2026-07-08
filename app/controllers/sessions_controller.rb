0eed528c9f55f557574af2e9b677b115b453c264
Mihara-Yamato-com <yamato-mihara@comthink.co.jp>
2026-07-08 13:02:20 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:email].to_s.downcase
+    password = params[:password]
+    
+    user = User.find_by(email: email)
+    if user && user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to user_path, notice: 'ログインに成功しました'
+    else
+      flash.now[:alert] = 'メールアドレスまたはパスワードが正しくありません。'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path, notice: "ログアウトしました"
+  end
+end

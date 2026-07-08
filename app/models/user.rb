4dbb68f79880b1039f489365b937d7cdef89f47a
Mihara-Yamato-com <yamato-mihara@comthink.co.jp>
2026-07-08 09:31:29 +0900

    Userモデルの作成とbcrypt追加

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    enum :role, { general: 0, admin: 1 }
+
+    validates :name, presence: true, length: {maximum: 50}
+    validates :email, presence: true, uniqueness: true, length: {maximum: 254}
+    validates :password, presence: true, length: {minimum: 8}
+end

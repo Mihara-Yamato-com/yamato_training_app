d3c5bb1f86c47c4e741c2c7c9e4aa55555b64a60
Mihara-Yamato-com <yamato-mihara@comthink.co.jp>
2026-07-09 16:53:29 +0900

    ユーザー編集機能とデータ削除

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,10 +14,20 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_users_path, notice: "ユーザー情報の更新に成功しました"
+      redirect_to admin_user_path, notice: "ユーザー情報の更新に成功しました"
     else
       flash.now[:alert] = 'ユーザー情報の更新に失敗しました。'
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      redirect_to admin_users_path, notice: "ユーザーを削除しました"
+    else
+      redirect_to admin_users_path, alert: "削除できませんでした"
     end
   end
 
